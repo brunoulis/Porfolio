ec9df030e2426f7240af2628fe1c6cfb299bc152
brunoulis <luisvapa2012@gmail.com>
2023-08-13 04:14:54 +0200

    13/08/23

--- script.js
+++ script.js
@@ -25,14 +25,15 @@ function efectoHabilidades() {
         let habilidades = document.getElementsByClassName("progreso");
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
+        habilidades[2].classList.add("paqueteoffice");
         habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[4].classList.add("java");
+        habilidades[5].classList.add("python");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("proyect");
     }
 }
 
