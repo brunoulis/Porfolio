fc62c61f04ad8573b7a6c8591fd69069e7c08a99
brunoulis <luisvapa2012@gmail.com>
2023-08-13 03:21:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let menuVisible = false;
+
+// Función que oculta o muestra el menú
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar() {
+    // Oculto el menú una vez que selecciono una opción
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+// Función que aplica las animaciones de las habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("wordpress");
+        habilidades[4].classList.add("drupal");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+    }
+}
+
+// Función para descargar el CV
+function descargarCV() {
+    var fileName = "BrunoVazquezCV.pdf";
+    var fileUrl = "img/brunoluisvazquezpaisCV.pdf";
+    
+    var a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
+
+// Detecto el scrolling para aplicar la animación de la barra de habilidades
+window.onscroll = function() {
+    efectoHabilidades();
+}
